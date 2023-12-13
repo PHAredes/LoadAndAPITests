9b4b284c7fb2c72f4cd161d541e79a887e80d0ef
pharedes <pedro.aredes@hotmail.com>
2023-12-13 00:07:50 -0300

    feat: script to generate mock data in csv format

---
+++ csv_generator.js
@@ -0,0 +1,37 @@
+const faker = require('faker');
+const fs = require('fs');
+
+// Função para gerar um registro aleatório
+function generateRandomRecord() {
+  return {
+    username: faker.internet.userName(),
+    title: faker.lorem.sentence(),
+    content: faker.lorem.paragraph(),
+  };
+}
+
+// Função para gerar um CSV com registros aleatórios
+function generateRandomCSV(numRecords) {
+  const records = [];
+
+  for (let i = 0; i < numRecords; i++) {
+    records.push(generateRandomRecord());
+  }
+
+  const csvContent = records.map(record =>
+    `${record.username},${record.title},${record.content}`
+  );
+
+  return csvContent.join('\n');
+}
+
+// Número de registros a serem gerados
+const numRecords = 10000;
+
+// Gera o conteúdo CSV
+const csvContent = generateRandomCSV(numRecords);
+
+// Escreve o conteúdo CSV em um arquivo
+fs.writeFileSync('./artillery_load_tests/users.csv', csvContent);
+
+console.log(`Arquivo CSV gerado com ${numRecords} registros.`);
